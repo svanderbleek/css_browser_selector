87f67fdcc5e8f4d5fecc8ffd7c95db6423da8510
retr0h <john@dewey.ws>
2009-12-14 17:38:23 -0800

    Ruby 1.9 compat

--- lib/action_view/helpers/css_browser_selector.rb
+++ lib/action_view/helpers/css_browser_selector.rb
@@ -108,12 +108,12 @@ module ActionView
             o = %W(opera opera#{$1})
             o << "mobile" if ua.include?('mini')
             o.join(" ")
-          when /webtv/ :              "gecko"
-          when /msie (\d)/ :          "ie ie#{$1}"
-          when %r{firefox/2} :        "gecko ff2"
-          when %r{firefox/3.5} :      "gecko ff3 ff3_5"
-          when %r{firefox/3} :        "gecko ff3"
-          when /konqueror/ :          "konqueror"
+          when /webtv/ ;              "gecko"
+          when /msie (\d)/ ;          "ie ie#{$1}"
+          when %r{firefox/2} ;        "gecko ff2"
+          when %r{firefox/3.5} ;      "gecko ff3 ff3_5"
+          when %r{firefox/3} ;        "gecko ff3"
+          when /konqueror/ ;          "konqueror"
           when /applewebkit\/([\d.]+).? \([^)]*\) ?(?:version\/(\d+))?.*$/
             o = %W(webkit)
             if ua.include?('iron')
@@ -124,7 +124,7 @@ module ActionView
               o << "safari safari"+ ($2 || (($1.to_i >= 400) ? '2' : '1'))
             end
             o.join(" ")
-          when /gecko/, /mozilla/ :   "gecko"
+          when /gecko/, /mozilla/ ;   "gecko"
         end
         os = ua.include?('mac') || ua.include?('darwin') ? ua.include?('iphone') ? 'iphone' : ua.include?('ipod') ? 'ipod' : 'mac' :
              ua.include?('x11') || ua.include?('linux') ? 'linux' :
